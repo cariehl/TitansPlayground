87eff932a43c724c61f1f75da158c882f63d6ae4
Cooper Riehl <cooper.riehl@gmail.com>
2023-02-07 19:26:33 -0500

    Add example for reading a file

--- FileAccessPlayground/src/App.java
+++ FileAccessPlayground/src/App.java
@@ -1,18 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) throws Exception {
+		// Prompt the user for their name, and write it to a file.
+		writeUserNameToFile();
+
+		// Open the file and display the contents.
+		displayFileContents();
+	}
+
+	private static void writeUserNameToFile() throws IOException {
 		Scanner scanner = new Scanner(System.in);
 		FileWriter writer = new FileWriter("data.txt", true);
 
 		System.out.println("What is your name?");
 		String name = scanner.nextLine();
 
-		// writer.write("Hello, " + name);
 		writer.append("Hello, " + name + "\n");
 
 		writer.close();
 		scanner.close();
+	}
+
+	private static void displayFileContents() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
+
+		System.out.println("The contents of data.txt are:");
+		String s;
+		while ((s = reader.readLine()) != null) {
+			System.out.println(s);
+		}
+
+		reader.close();
 	}
 }
